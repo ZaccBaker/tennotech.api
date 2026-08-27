28d6ff98a103aba211d93b9336557ec5e496705f
ZaccBaker <zac.baker232@gmail.com>
2026-08-27 16:52:31 -0500

    updated server/port

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
-    console.log(`API running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`API running on port ${PORT}`);
 });
